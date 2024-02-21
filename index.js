7a3d319cb6549260553f9d2381b78e12cbb10227
Taranjeet Singh <sin12506@sheridancollege.ca>
2024-02-20 21:39:53 -0500

    added bookmark functionality

---
+++ index.js
@@ -0,0 +1,6 @@
+let bookmarkButton = document.querySelector(".bookmark-button");
+
+// bookmark effect
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark-activated");
+});
