710e5b3d221c4c1fdf4ac4da2e2ce16434cf1200
Taranjeet Singh <sin12506@sheridancollege.ca>
2024-03-16 22:29:43 -0400

    added javascript and css for project info

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ let bookmarkButton = document.querySelector(".bookmark-button");
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark-activated");
 });
+
+let backProject = document.querySelector(".back-project-button");
+let backProjectinfo = document.querySelector(".back-this-project");
+let closeProjectinfo = document.querySelector(".close-project-info");
+
+// add opening effect when button clicked
+
+// improve this code by adding class then add toggle effect on that class
+
+backProject.addEventListener("click", () => {
+  backProjectinfo.style.visibility = "visible";
+});
+
+closeProjectinfo.addEventListener("click", () => {
+  backProjectinfo.style.visibility = "hidden";
+});
