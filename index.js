e2d9afcd51d361341f81e07f02afa1738e738957
Taranjeet Singh <sin12506@sheridancollege.ca>
2024-03-21 01:22:13 -0400

    added jquery blur effect

--- index.js
+++ index.js
@@ -9,14 +9,39 @@ let backProject = document.querySelector(".back-project-button");
 let backProjectinfo = document.querySelector(".back-this-project");
 let closeProjectinfo = document.querySelector(".close-project-info");
 
-// add opening effect when button clicked
-
-// improve this code by adding class then add toggle effect on that class
-
 backProject.addEventListener("click", () => {
   backProjectinfo.style.visibility = "visible";
 });
 
 closeProjectinfo.addEventListener("click", () => {
   backProjectinfo.style.visibility = "hidden";
+});
+
+// plan to hide rewards entrance amount until it get selected
+
+$(".back-project-button").click(function () {
+  $(".main , aside, .navbar-default").addClass("blur");
+  $(".back-this-project").addClass("fullOpacity");
+});
+
+$(closeProjectinfo).click(function () {
+  $(".main , aside , .navbar-default").removeClass("blur");
+});
+
+// showing input for entering pledge amount
+
+$(".rewardsPledge").change(function () {
+  let isChecked = $(this).is(":checked");
+
+  if (isChecked) {
+    console.log("changed event if  triggered");
+    $(this)
+      .closest(".rewards")
+      .find(".beforeSelect")
+      .removeClass("beforeSelect");
+  } else {
+    // never occurred
+    console.log("changed event else statment triggered");
+    $(this).closest(".rewards").find(".beforeSelect").addClass("beforeSelect");
+  }
 });
